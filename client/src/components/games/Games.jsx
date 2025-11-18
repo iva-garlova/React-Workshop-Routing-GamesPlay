81ba89d52d9bd181fcd6e4704081d7740c37c63e
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-11-18 15:16:06 +0200

    list games in catalog page

--- client/src/components/games/Games.jsx
+++ client/src/components/games/Games.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import gameServise from "../../services/gameServise";
+import GameItem from "./game-item/GameItem";
 
 export default function Games() {
     const [games, setGames] = useState([]);
@@ -8,36 +9,12 @@ export default function Games() {
         gameServise.getAll()
         .then(setGames)
     }, [])
+
     return (
           <section id="catalog-page">
             <h1>All Games</h1>
             {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="/images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+     {games.map(game => <GameItem key={game._id} {...game}/>)}
 
             {/* <!-- Display paragraph: If there is no games  --> */}
             <h3 className="no-articles">No articles yet</h3>
