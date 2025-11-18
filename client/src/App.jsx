469e2d1fd7e08b5e1cdd883c3285b58ed1c26862
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-11-18 12:08:03 +0200

    extract few components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/home/Home'
+
 
 function App() {
-  
+
   return (
     <div id='box'>
+      <Header />
 
-     <h1>Games Play</h1>
-     
+      <main id="main-content">
+        <Home />
+      </main>
     </div>
   )
 }
