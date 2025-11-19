0287b21105c9e5b89fbe5ca2b403ea8954167815
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2025-11-19 11:06:01 +0200

    show edit page with the prepopulated values in the form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/games/create' element={<GameCreate />} />
           <Route path='/games/:gameId/details' element={<GameDetails />} />
+          <Route path='/games/:gameId/edit' element={<GameEdit />} />
           <Route path='/games/edit' element={<GameEdit />} />
           <Route path='/games' element={<Games />} />
         </Routes>
