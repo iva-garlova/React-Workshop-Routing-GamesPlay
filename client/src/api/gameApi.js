2c47dc24b3ff151743f44e6d30873507c463829f
iva-garlova <139470536+iva-garlova@users.noreply.github.com>
2026-02-03 17:58:50 +0200

    select properties

--- client/src/api/gameApi.js
+++ client/src/api/gameApi.js
@@ -71,6 +71,7 @@ export const useLatestGames = () => {
     const searchParams = new URLSearchParams({
     sortBy: '_createdOn desc',
     pageSize: PAGE_SIZE,
+    select: '_id,imageUrl,title'
 
   })
     request.get(`${baseUrl}?${searchParams.toString()}`)
